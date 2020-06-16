d4a21466b7045e90719d8ca22ca07a35a21a8de8
Alexander Profeit <alex@profeit.com>
2020-06-16 02:00:56 -0400

    Add icons and all guests

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 import Home from './components/Home';
 import NewGuest from './components/NewGuest';
+import AllGuests from './components/AllGuests';
 
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
@@ -23,16 +24,12 @@ export default function App() {
           name="NewGuest"
           component={NewGuest}
         />
+        <Stack.Screen
+          name="AllGuests"
+          component={AllGuests}
+          options={{ title: 'All Guests' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
--- components/Home.js
+++ components/Home.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import { Button, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Home({ navigation }) {
   return (
-    <>
-    <Button
-      onPress={
-        () => {
-          navigation.navigate('NewGuest')
+    <View style={styles.container}>
+      <Button
+        onPress={
+          () => {
+            navigation.navigate('NewGuest')
+          }
         }
-      }
-      title="New Guest"
-    />
-    </>
+        title="New Guest"
+        icon={
+          <Icon name="user-plus" style={styles.icon} />
+        }
+        type="clear"
+      />
+      <Button
+        onPress={
+          () => {
+            navigation.navigate('AllGuests')
+          }
+        }
+        title="All Guests"
+        icon={
+          <Icon name="lock" style={styles.icon} />
+        }
+        type="clear"
+      />
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  icon: {
+    marginRight: 10,
+    fontSize: 20,
+    color: 'rgb(32, 137, 220)',
+  }
+});
