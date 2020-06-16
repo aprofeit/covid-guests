cb2a5a6064a7563a3f6b490a7ab1c02a4a8de2f4
Alexander Profeit <alex@profeit.com>
2020-06-16 17:42:09 -0400

    Reorganize project

--- App.js
+++ App.js
@@ -1,36 +1,9 @@
 import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 import * as eva from '@eva-design/eva';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { ApplicationProvider, BottomNavigation, BottomNavigationTab } from '@ui-kitten/components';
+import { ApplicationProvider } from '@ui-kitten/components';
 
-import NewGuest from './components/NewGuest';
-import Settings from './components/Settings';
-
-const { Navigator, Screen } = createBottomTabNavigator();
-
-const BottomTabBar = ({ navigation, state }) => (
-  <BottomNavigation
-    selectedIndex={state.index}
-    onSelect={index => navigation.navigate(state.routeNames[index])}>
-    <BottomNavigationTab title='NEW GUEST'/>
-    <BottomNavigationTab title='SETTINGS'/>
-  </BottomNavigation>
-);
-
-const TabNavigator = () => (
-  <Navigator tabBar={props => <BottomTabBar {...props} />}>
-    <Screen name='New Guest' component={NewGuest}/>
-    <Screen name='Settings' component={Settings}/>
-  </Navigator>
-);
-
-const AppNavigator = () => (
-  <NavigationContainer>
-    <TabNavigator/>
-  </NavigationContainer>
-);
+import AppNavigator from './components/AppNavigator';
 
 export default function App() {
   return (
